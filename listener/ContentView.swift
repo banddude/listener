c2c733f6248ec4544a307ba0ea42516a4b80edd1
banddude <your-email@example.com>
2025-05-23 10:49:28 -0700

    Initial Commit

---
+++ listener/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  listener
+//
+//  Created by Mike Shaffer on 5/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ listener/listenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  listenerApp.swift
+//  listener
+//
+//  Created by Mike Shaffer on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct listenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
